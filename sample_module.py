4faf4337485993d16fc184bf619b81304b1bd8a8
Brian Clements <brian.l.clements03@gmail.com>
2021-10-07 12:27:08 -0500

    add content to exercises

---
+++ sample_module.py
@@ -0,0 +1,12 @@
+import math
+
+def double_the_root(num):
+    return math.sqrt(num) * 2
+
+def add_two(num):
+    return num * 2
+
+def subtract_two(num):
+    return num - 2
+
+bootcamp_name = 'Codeup'
